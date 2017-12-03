15094fdc1e1d2149dd71529a9e44c7a0609b9174
Nikhil Cherukuri <nikhilcherukuri@Nikhils-MacBook-Pro.local>
2017-12-03 14:49:31 -0800

    can add listings to database

--- app/src/main/java/itp341/cherukuri/nikhil/trojanbooks/DetailsActivity.java
+++ app/src/main/java/itp341/cherukuri/nikhil/trojanbooks/DetailsActivity.java
@@ -7,20 +7,23 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseUser;
 import com.squareup.picasso.Picasso;
 
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.ChildEventListener;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class DetailsActivity extends AppCompatActivity {
 
     public static final String DETAILS_CODE = "itp341.cherukuri.nikhil.trojanbooks.detailscode";
-    DatabaseReference mDB;
+
+    FirebaseAuth mAuth;
+    FirebaseUser user;
+    DatabaseReference mDatabase;
     DatabaseReference mListItemRef;
 
     ImageView imageView;
@@ -31,19 +34,21 @@ public class DetailsActivity extends AppCompatActivity {
     EditText etListPrice;
     EditText etClassCode;
     Button button;
+    RadioGroup group;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
-        mDB= FirebaseDatabase.getInstance().getReference();
-        mListItemRef = mDB.child("listing");
-
+        mAuth = FirebaseAuth.getInstance();
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        mListItemRef = mDatabase.child("listing");
+         user = mAuth.getCurrentUser();
         Intent i = getIntent();
         int position = i.getIntExtra(DETAILS_CODE,-1);
 
         if(position == -1)
             return;
-        Book book = BookSingleton.getInstance().getBookAtIndex(position);
+        final Book book = BookSingleton.getInstance().getBookAtIndex(position);
 
         imageView = (ImageView) findViewById(R.id.details_imageview);
         etBookName = (EditText) findViewById(R.id.detailsBookName);
@@ -53,7 +58,7 @@ public class DetailsActivity extends AppCompatActivity {
         etListPrice = (EditText) findViewById(R.id.detailsListPrice);
         etClassCode = (EditText) findViewById(R.id.classCode);
         button = (Button) findViewById(R.id.buttonFinish);
-
+        group = (RadioGroup) findViewById(R.id.radioGroup);
 
         Picasso.with(getApplicationContext()).
                 load(book.getImageURL()).
@@ -67,7 +72,25 @@ public class DetailsActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                String bookName = etBookName.getText().toString();
+                String authorName = etAuthorName.getText().toString();
+                String ISBN = etISBN.getText().toString();
+                String googlePrice = etPriceOnGoogle.getText().toString();
+                String listPrice = etListPrice.getText().toString();
+                String classCode = etClassCode.getText().toString();
+                String imageUrl = book.getImageURL();
+                boolean buying = (group.getCheckedRadioButtonId() == R.id.buyingButton);
+                String userID = user.getUid();
+                if(listPrice.isEmpty()){
+                    Toast.makeText(getApplicationContext(),"Please List the price that you want to sell at", Toast.LENGTH_LONG).show();
+                    return;
+                }
 
+                if(classCode.isEmpty())
+                    classCode = "N/A";
+
+                Listing listing = new Listing(bookName,authorName,ISBN,googlePrice,listPrice,classCode, imageUrl,buying, userID);
+                mDatabase.child("listings").child(userID).setValue(listing);
             }
         });
     }
--- app/src/main/java/itp341/cherukuri/nikhil/trojanbooks/Listing.java
+++ app/src/main/java/itp341/cherukuri/nikhil/trojanbooks/Listing.java
@@ -1,57 +1,86 @@
 package itp341.cherukuri.nikhil.trojanbooks;
 
-import java.util.Date;
-
 /**
- * Created by nikhilcherukuri on 11/20/17.
+ * Created by nikhilcherukuri on 12/3/17.
  */
 
-
 public class Listing {
-    private Book book;
-    double price;
-    User user;
-    Date date;
-    boolean buying;
+    private String bookName;
+    private String auhtorName;
+    private String ISBN;
+    private String priceOnGoogle;
+    private String listPrice;
+    private String classCode;
+    private String imageURL;
+    public String userID;
+    private boolean buying;
 
-    public Listing(Book book, double price, User user, Date date, boolean buying) {
-        this.book = book;
-        this.price = price;
-        this.user = user;
-        this.date = date;
+    public Listing(String bookName, String auhtorName, String ISBN, String priceOnGoogle, String listPrice, String classCode, String imageURL, boolean buying, String userID) {
+        this.bookName = bookName;
+        this.auhtorName = auhtorName;
+        this.ISBN = ISBN;
+        this.priceOnGoogle = priceOnGoogle;
+        this.listPrice = listPrice;
+        this.classCode = classCode;
+        this.imageURL = imageURL;
         this.buying = buying;
+        this.userID = userID;
     }
 
-    public Book getBook() {
-        return book;
+    public String getBookName() {
+        return bookName;
     }
 
-    public void setBook(Book book) {
-        this.book = book;
+    public void setBookName(String bookName) {
+        this.bookName = bookName;
     }
 
-    public double getPrice() {
-        return price;
+    public String getAuhtorName() {
+        return auhtorName;
     }
 
-    public void setPrice(double price) {
-        this.price = price;
+    public void setAuhtorName(String auhtorName) {
+        this.auhtorName = auhtorName;
     }
 
-    public User getUser() {
-        return user;
+    public String getISBN() {
+        return ISBN;
     }
 
-    public void setUser(User user) {
-        this.user = user;
+    public void setISBN(String ISBN) {
+        this.ISBN = ISBN;
     }
 
-    public Date getDate() {
-        return date;
+    public String getPriceOnGoogle() {
+        return priceOnGoogle;
     }
 
-    public void setDate(Date date) {
-        this.date = date;
+    public void setPriceOnGoogle(String priceOnGoogle) {
+        this.priceOnGoogle = priceOnGoogle;
+    }
+
+    public String getListPrice() {
+        return listPrice;
+    }
+
+    public void setListPrice(String listPrice) {
+        this.listPrice = listPrice;
+    }
+
+    public String getClassCode() {
+        return classCode;
+    }
+
+    public void setClassCode(String classCode) {
+        this.classCode = classCode;
+    }
+
+    public String getImageURL() {
+        return imageURL;
+    }
+
+    public void setImageURL(String imageURL) {
+        this.imageURL = imageURL;
     }
 
     public boolean isBuying() {
@@ -60,5 +89,28 @@ public class Listing {
 
     public void setBuying(boolean buying) {
         this.buying = buying;
+    }
+
+    public String getUserID() {
+        return userID;
+    }
+
+    public void setUserID(String userID) {
+        this.userID = userID;
+    }
+
+    @Override
+    public String toString() {
+        return "Listing{" +
+                "bookName='" + bookName + '\'' +
+                ", auhtorName='" + auhtorName + '\'' +
+                ", ISBN='" + ISBN + '\'' +
+                ", priceOnGoogle='" + priceOnGoogle + '\'' +
+                ", listPrice='" + listPrice + '\'' +
+                ", classCode='" + classCode + '\'' +
+                ", imageURL='" + imageURL + '\'' +
+                ", buying=" + buying + '\'' +
+                ", userID=" + userID +
+                '}';
     }
 }
